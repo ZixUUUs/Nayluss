9f13f4424c66ab3259726ae11ec224a0264fbc04
ZixUUUs <161074193+ZixUUUs@users.noreply.github.com>
2025-06-03 16:07:09 +0100

    adding features to offres like supplement and working on the display

--- assets/Main.js
+++ assets/Main.js
@@ -9,7 +9,34 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElem = document.querySelectorAll(".hidden");
 hiddenElem.forEach((el) => observer.observe(el));
-// slimselect //
-new SlimSelect({
-  select: "#multiple",
-});
+
+// select behavior and display option //
+const select = document.querySelector("#supplement");
+const options = document.querySelectorAll("option");
+const displayOption = document.querySelector(".display-option ul");
+
+function getOpt() {
+  select.addEventListener("change", (event) => {
+    const selectedValue = event.target.value;
+    console.log(selectedValue);
+
+    const list = document.createElement("li");
+    list.textContent = selectedValue;
+    const closeBtn = document.createElement("button");
+    closeBtn.textContent = "X";
+    list.appendChild(closeBtn);
+    displayOption.appendChild(list);
+
+    const selectedOption = select.querySelector(
+      `option[value="${selectedValue}"]`
+    );
+    selectedOption.disabled = true;
+
+    closeBtn.addEventListener("click", () => {
+      list.remove();
+
+      selectedOption.disabled = false;
+    });
+  });
+}
+getOpt();
